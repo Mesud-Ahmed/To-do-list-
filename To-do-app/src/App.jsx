8f822ca3afc1af2e7b41f29d224c430da66c7d78
Mesud-Ahmed <mesudahmed94@gmail.com>
2024-11-03 15:20:28 +0300

    add todo list and todo item components

---
+++ To-do-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+import  CssBaseline  from '@mui/material/CssBaseline'
+import TodoList from './Todo/TodoList'
+function App() {
+
+  return (
+    <div >
+      <CssBaseline />
+      <TodoList />
+    </div>
+  )
+}
+
+export default App
